a5e833d191894435538e9b81ae44f11672fc88bb
Tobia Rossi <github@tobiarossi.ch>
2022-04-27 17:25:37 +0200

    Corrected wrongly formatted command name

--- commands/bwisi_time.js
+++ commands/bwisi_time.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('Bwisi_Time')
+		.setName('bwisi_time')
 		.setDescription('Ti dice una frase per la bwisa!'),
 	async execute(interaction) {
 		return interaction.reply('Calma ci sto lavorando!');
